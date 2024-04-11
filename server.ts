a119ffc19f58e69ead463297b6fbbe19ea3a5493
mredaht <687099742@uma.es>
2024-04-11 19:01:13 +0200

    nada importante

--- server.ts
+++ server.ts
@@ -5,7 +5,6 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 
-// Se exporta la aplicación Express para que pueda ser utilizada por funciones sin servidor.
 export function aplicacion(): express.Express {
   const servidor = express();
   const carpetaDistribucionServidor = dirname(fileURLToPath(import.meta.url));
@@ -17,14 +16,11 @@ export function aplicacion(): express.Express {
   servidor.set('view engine', 'html');
   servidor.set('views', carpetaDistribucionNavegador);
 
-  // Ejemplo de puntos finales de API Rest de Express
-  // server.get('/api/**', (req, res) => { });
-  // Servir archivos estáticos desde /navegador
+  
   servidor.get('*.*', express.static(carpetaDistribucionNavegador, {
     maxAge: '1y'
   }));
 
-  // Todas las rutas regulares utilizan el motor Angular
   servidor.get('*', (req, res, next) => {
     const { protocol, originalUrl, baseUrl, headers } = req;
 
@@ -43,7 +39,7 @@ export function aplicacion(): express.Express {
   return servidor;
 }
 
-function iniciar(): void {
+function start(): void {
   const puerto = process.env['SSR_PORT'] || 4000;
 
   // Iniciar el servidor Node
@@ -53,4 +49,4 @@ function iniciar(): void {
   });
 }
 
-iniciar();
+start();
