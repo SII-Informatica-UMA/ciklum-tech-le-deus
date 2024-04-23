caf30637272401e77231d9169ce022845975772f
mredaht <687099742@uma.es>
2024-04-23 16:57:11 +0200

    entities

---
+++ entidades/src/test/java/ciklumtechledeus/entidades/EntidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package ciklumtechledeus.entidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
