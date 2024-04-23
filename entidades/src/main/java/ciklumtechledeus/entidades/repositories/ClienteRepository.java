7431265bc51c13c5e975703ab7d26b6b3cfbc1de
mredaht <687099742@uma.es>
2024-04-23 18:08:24 +0200

    Create ClienteRepository.java

---
+++ entidades/src/main/java/ciklumtechledeus/entidades/repositories/ClienteRepository.java
@@ -0,0 +1,7 @@
+package ciklumtechledeus.entidades.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepository extends JpaRepository{
+    
+}
