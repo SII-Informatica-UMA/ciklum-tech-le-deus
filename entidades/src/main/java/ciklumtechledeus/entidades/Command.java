3f9b13ede3e60fb60559abfa0089caf744d4b37a
mredaht <687099742@uma.es>
2024-04-24 19:25:15 +0200

    create Command.java

---
+++ entidades/src/main/java/ciklumtechledeus/entidades/Command.java
@@ -0,0 +1,35 @@
+package ciklumtechledeus.entidades;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+import ciklumtechledeus.entidades.entities.Dieta;
+
+
+import ciklumtechledeus.entidades.repositories.DietaRepository;
+
+import jakarta.transaction.Transactional;
+@Component
+public class Command implements CommandLineRunner {
+    
+    private DietaRepository dietaRepository;
+
+    public Command(DietaRepository dietaRepository) {
+        this.dietaRepository = dietaRepository;
+    }
+    
+    @Override
+    @Transactional
+    public void run(String... args) throws Exception {
+        for (String s: args) {
+            System.out.println(s);
+        }
+        if(args.length > 0){
+            for(Dieta d : dietaRepository.findByNombre(args[0])){
+                System.out.println(d);
+            }
+        }
+        
+    }
+}
--- entidades/src/main/java/ciklumtechledeus/entidades/repositories/DietaRepository.java
+++ entidades/src/main/java/ciklumtechledeus/entidades/repositories/DietaRepository.java
@@ -8,11 +8,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public interface DietaRepository extends JpaRepository<Dieta, Integer> {
+    @Query("INSERT INTO Dieta(nombre, descripcion, observaciones, objetivo, duracionDias, alimentos, recomendaciones, entrenadorId, clienteId) VALUES (:nombre, :descripcion, :observaciones, :objetivo, :duracionDias, :alimentos, :recomendaciones, :entrenadorId, :clienteId)")
+    void insertDieta(@Param("nombre") String nombre, @Param("descripcion") String descripcion, @Param("observaciones") String observaciones, @Param("objetivo") String objetivo, @Param("duracionDias") int duracionDias, @Param("alimentos") ArrayList<String> alimentos, @Param("recomendaciones") String recomendaciones, @Param("entrenadorId") Long entrenadorId, @Param("clienteId") Set<Long> clienteId);
+    @Query("UPDATE Dieta SET nombre = :nombre, descripcion = :descripcion, observaciones = :observaciones, objetivo = :objetivo, duracionDias = :duracionDias, alimentos = :alimentos, recomendaciones = :recomendaciones, clienteId = :clienteId WHERE id = :id AND entrenadorId = :entrenadorId")
+    void updateDieta(@Param("nombre") String nombre, @Param("descripcion") String descripcion, @Param("observaciones") String observaciones, @Param("objetivo") String objetivo, @Param("duracionDias") int duracionDias, @Param("alimentos") ArrayList<String> alimentos, @Param("recomendaciones") String recomendaciones, @Param("entrenadorId") Long entrenadorId, @Param("clienteId") Set<Long> clienteId,@Param("id") Long id);
+    @Query("UPDATE Dieta SET clienteId = :clienteId WHERE id = :id AND entrenadorId = :entrenadorId")
+    void updateCliente(@Param("entrenadorId") Long entrenadorId, @Param("clienteId") Set<Long> clienteId,@Param("id") Long id);
     
-    @Query( "Select d from Dieta d where d.usuarioId = :nombre" )
-    List<Dieta> findByIdUsuaraio(@Param("nombre") String nombre);
-
+    List<Dieta> findByClienteId(Set<Long> clienteId);
+    List<Dieta> findByEntrenadorId(Long entrenadorid);
+    List<Dieta> findByNombre(String nombre);
 }
