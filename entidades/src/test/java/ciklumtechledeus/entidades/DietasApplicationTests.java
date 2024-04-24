07d8420043f729a89211049ceabf87bd5adb1449
mredaht <687099742@uma.es>
2024-04-24 21:23:57 +0200

    nada importante

--- entidades/src/test/java/ciklumtechledeus/entidades/DietasApplicationTests.java
+++ entidades/src/test/java/ciklumtechledeus/entidades/DietasApplicationTests.java
@@ -1,15 +1,14 @@
-/*package ciklumtechledeus.entidades;
+package ciklumtechledeus.entidades;
 
-import org.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.*;
+
+
 @SpringBootTest
-class GestionDietasApplicationTests {
+class DietasApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
 }
-*/
