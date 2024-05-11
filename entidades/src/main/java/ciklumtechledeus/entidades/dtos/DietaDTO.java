e667bc4cdd537bda43886cd0bc2f4a4a774e9589
mredaht <687099742@uma.es>
2024-05-11 17:44:58 +0200

    Create DietaDTO.java

---
+++ entidades/src/main/java/ciklumtechledeus/entidades/dtos/DietaDTO.java
@@ -0,0 +1,18 @@
+package ciklumtechledeus.entidades.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class DietaDTO {
+    private Long id;
+}
