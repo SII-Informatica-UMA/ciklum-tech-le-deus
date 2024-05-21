b291ecd4caa3b3ab8dcd199c934cb34ded38fc18
mredaht <687099742@uma.es>
2024-05-21 18:16:55 +0200

    Update Mapper.java

--- entidades/src/main/java/ciklumtechledeus/entidades/controllers/Mapper.java
+++ entidades/src/main/java/ciklumtechledeus/entidades/controllers/Mapper.java
@@ -6,20 +6,10 @@ import ciklumtechledeus.entidades.entities.Dieta;
 
 public class Mapper {
 
-    
-    public static Dieta toDieta(DietaNuevaDTO dietaNuevaDTO) {
-        return ciklumtechledeus.entidades.entities.Dieta.builder()
-            .nombre(dietaNuevaDTO.getNombre())
-            .descripcion(dietaNuevaDTO.getDescripcion())
-            .observaciones(dietaNuevaDTO.getObservaciones())
-            .objetivo(dietaNuevaDTO.getObjetivo())
-            .duracionDias(dietaNuevaDTO.getDuracionDias())
-            .alimentos(dietaNuevaDTO.getAlimentos())
-            .recomendaciones(dietaNuevaDTO.getRecomendaciones())
-            .build();
-    }
-
     public static DietaDTO toDietaDTO(Dieta dieta) {
+        //Convierte una entidad dieta en un objeto DTO
+        //Util para devolver informacion sobre una dieta 
+        //desde la capa de servicio para la de presentacion
         return DietaDTO.builder()
             .id(dieta.getId())
             .nombre(dieta.getNombre())
@@ -31,6 +21,29 @@ public class Mapper {
             .recomendaciones(dieta.getRecomendaciones())
             .build();
     }
+
+    public static Dieta toDietaNueva(DietaNuevaDTO dietaNuevaDTO) {
+        // Convierte un objeto DietaNuevaDTO en una entidad Dieta
+        // Útil para recibir datos desde la capa de presentación y convertirlos en una entidad manejable por la capa de servicio
+        return Dieta.builder()
+            .nombre(dietaNuevaDTO.getNombre())
+            .descripcion(dietaNuevaDTO.getDescripcion())
+            .observaciones(dietaNuevaDTO.getObservaciones())
+            .objetivo(dietaNuevaDTO.getObjetivo())
+            .duracionDias(dietaNuevaDTO.getDuracionDias())
+            .alimentos(dietaNuevaDTO.getAlimentos())
+            .recomendaciones(dietaNuevaDTO.getRecomendaciones())
+            .build();
+    }
+
+    public static Dieta toDieta(DietaDTO dietaDTO) {
+        // Convierte un objeto DietaDTO en una entidad Dieta
+        // Útil para recibir datos desde la capa de presentación y convertirlos en una entidad manejable por la capa de servicio
+        return Dieta.builder()
+            .id(dietaDTO.getId())
+            .build();
+    }
+    
 
 
 }
