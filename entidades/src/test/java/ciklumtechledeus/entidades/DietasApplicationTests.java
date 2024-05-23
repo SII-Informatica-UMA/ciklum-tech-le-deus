7046b53f14cb5fdbc300245c5d04e482f9b848e0
mredaht <687099742@uma.es>
2024-05-23 12:14:17 +0200

    Update DietasApplicationTests.java

--- entidades/src/test/java/ciklumtechledeus/entidades/DietasApplicationTests.java
+++ entidades/src/test/java/ciklumtechledeus/entidades/DietasApplicationTests.java
@@ -1,11 +1,94 @@
 package ciklumtechledeus.entidades;
 
+import java.net.URI;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
+import org.springframework.web.util.UriBuilderFactory;
 
+import ciklumtechledeus.entidades.entities.Dieta;
+import ciklumtechledeus.entidades.repositories.DietaRepository;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DisplayName("En el servicio de dietas")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class DietasApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Value(value = "${local.server.port}")
+	private int port;
+
+	@Autowired
+	private DietaRepository repoDieta;
+
+	@BeforeEach
+	public void initializeDatabase() {
+		repoDieta.deleteAll();
+	}
+
+	private URI uri(String scheme, String host, int port, String... paths) {
+		UriBuilderFactory ubf = new DefaultUriBuilderFactory();
+        UriBuilder ub = ubf.builder()
+            .scheme(scheme)
+            .host(host).port(port);
+        for (String path : paths) {
+            ub = ub.path(path);
+        }
+        return ub.build();
+	}
+
+	private RequestEntity<Void> get(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.get(uri)
+            .accept(MediaType.APPLICATION_JSON)
+            .build();
+        return peticion;
+    }
+
+	private RequestEntity<Void> delete(String scheme, String host, int port, String path) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.delete(uri)
+            .build();
+        return peticion;
+    }
+
+	private <T> RequestEntity<T> post(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.post(uri)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(object);
+        return peticion;
+    }
+
+	private <T> RequestEntity<T> put(String scheme, String host, int port, String path, T object) {
+        URI uri = uri(scheme, host, port, path);
+        var peticion = RequestEntity.put(uri)
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(object);
+        return peticion;
+    }
+
+	private void compruebaCampos(Dieta expected, Dieta actual){
+		assertThat(actual.getId()).isEqualTo(expected.getId());
+//		assertThat(actual.getClienteId()).isEqualTo(expected.getClienteId());
+//		assertThat(actual.getEntrenadorId()).isEqualTo(expected.getClienteId());
+	}
+
 
 	@Test
 	void contextLoads() {
